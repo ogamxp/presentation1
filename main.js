e4b04923a3085891403f1e84ae06907a26e87a3f
ogamxp <xyliganistan100mk@gmail.com>
2023-05-11 16:39:21 +0500

    second commit

---
+++ main.js
@@ -0,0 +1,26 @@
+// 1 function declaration
+// function declaration(a, b, c) {
+//     return a + b + c
+// }
+
+// console.log(declaration(1, 2, 3))
+
+// 2 function expression
+
+//2.1 arrow function
+// let expression = (a)=> {
+//     return a
+// }
+// console.log(expression(5))
+
+// 2.2 anonymous function
+// let expression2 = function(b) {
+//     return b + 2
+// }
+
+// console.log(expression2(5))
+
+// IIFE
+// (function(f) {
+//     console.log(4)
+// })()
